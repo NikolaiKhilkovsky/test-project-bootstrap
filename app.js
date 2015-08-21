43d5384bc2a6b549fefa5edf6485bdd9ca82b03f
Dima Chukhai <dippdc@gmail.com>
2015-08-21 16:51:26 +0300

    Improve mock service

--- app.js
+++ app.js
@@ -2,34 +2,37 @@
 
 angular.module('app', ['httpAPIMock'])
     .controller('ApiTestController', function ($scope, $http) {
-        $http.get('/api/files')
+        function logExample(title, promise) {
+            return promise.then(function (response) {
+                console.log(title, '\n', response.config.method, response.config.url, response.config.data, response.status, '\nResponse:', response.data);
+                return response;
+            }).catch(function(response){
+                console.error(title, '\n', response.config.method, response.config.url, response.config.data, response.status, '\nResponse:', response.data);
+            })
+        }
+
+        logExample('Rename',
+            $http.post('/api/files/rename', {from: '/image-2.jpg', to: '/image-3.jpg'})
+        );
+        logExample('Rename directory',
+            $http.post('/api/files/rename', {from: '/my-vacation', to: '/vacation'})
+        );
+        logExample('Rename: source file doesn\'t exist',
+            $http.post('/api/files/rename', {from: '/image-does-not-exist.jpg', to: '/image-2.jpg'})
+        );
+        logExample('Rename: Target file already exists',
+            $http.post('/api/files/rename', {from: '/image-0.jpg', to: '/image-10.jpg'})
+        );
+
+        logExample('List root directory', $http.get('/api/files'))
             .then(function (response) {
-                console.log('List', response.data);
                 $scope.files = response.data;
             });
 
-        $http.get('/api/files?dir=/my-vacation')
-            .then(function (response) {
-                console.log('List', response.data);
-            })
-            .catch(function (response) {
-                console.error('list', response);
-            });
-
-        $http.post('/api/files/rename', {from: '/image-2.jpg', to: '/image-3.jpg'})
-            .then(function (response) {
-                console.log('Rename', response.data);
-            });
-
-        //Source file doesn't exist error
-        $http.post('/api/files/rename', {from: '/image-does-not-exist.jpg', to: '/image-2.jpg'})
-            .catch(function (response) {
-                console.error('Rename', response);
-            });
-
-        //Target file already exists error
-        $http.post('/api/files/rename', {from: '/image-0.jpg', to: '/image-10.jpg'})
-            .catch(function (response) {
-                console.error('Rename', response);
-            });
+        logExample('List directory',
+            $http.get('/api/files?path=/vacation')
+        );
+        logExample('List single file',
+            $http.get('/api/files?path=/image-0.jpg')
+        );
     });
